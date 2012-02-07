b2cc2f134c85893a2d4448f25014d71122ec1628
Eck Doerry <eckdoerry@gmail.com>
2012-02-07 11:54:33 -0700

    tried a different grid size

--- src/com/android/eckdoerry/ImageAdapter.java
+++ src/com/android/eckdoerry/ImageAdapter.java
@@ -31,7 +31,7 @@ public class ImageAdapter extends BaseAdapter {
         ImageView imageView;
         if (convertView == null) {  // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setLayoutParams(new GridView.LayoutParams(95, 95));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(8, 8, 8, 8);
         } else {
